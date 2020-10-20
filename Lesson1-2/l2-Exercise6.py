16154adf19551ce25aaca095ac5aaf2cb849569f
Harini Vaidyanathan <harinivaidy2001@gmail.com>
2020-10-20 03:19:37 -0700

    Adding exercise 6 and file

---
+++ Lesson1-2/l2-Exercise6.py
@@ -0,0 +1,23 @@
+
+
+from netmiko import Netmiko
+from getpass import getpass
+
+password = getpass()
+device = {
+    "host": "cisco4.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "secret": password,
+    "device_type": "cisco_ios",
+    "session_log": "my_output.txt",
+}
+
+
+net_connection = Netmiko(**device)
+print(net_connection.find_prompt())
+
+net_connection.config_mode()
+print(net_connection.find_prompt())
+net_connection.exit_config_mode()
+print(net_connection.find_prompt())
