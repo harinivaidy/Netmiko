6d31038fcb7a0c9f5db4d946b25fe78912052bea
Harini Vaidyanathan <harinivaidy2001@gmail.com>
2020-10-24 04:50:21 -0700

    Adding Exercise2 files

---
+++ Lesson3/Lesson3-Exercise2a.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+import yaml
+
+new_list = []
+
+device1 = {
+
+'username':'harinipython',
+'password':'joker',
+'device_type':'cisco_ios',
+'host':'cisco3.lasthop.io'
+}
+
+device2 = {
+
+'username':'harinipython',
+'password':'joker',
+'device_type':'arista',
+'host':'arista1.lasthop.io'
+
+}
+
+device3 = {
+
+'username':'harinipython',
+'password':'joker',
+'device_type':'arista',
+'host':'arista2.lasthop.io'
+
+}
+
+device4 = {
+
+'username':'harinipython',
+'password':'joker',
+'device_type':'juniper_junos',
+'host':'srx2.lasthop.io'
+
+}
+
+new_list.append(device1)
+new_list.append(device2)
+new_list.append(device3)
+new_list.append(device4)
+
+
+pprint(new_list)
+
+device1['secret'] = '8982'
+
+f = open("output_file_in_yaml.yml", "w")
+yaml.dump(new_list, f, default_flow_style=True)
+
