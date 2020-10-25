67683bc3c7ebd3e4b7723b02ae23b8803249d562
Harini Vaidyanathan <harinivaidy2001@gmail.com>
2020-10-25 11:19:10 -0700

    Updating Exercise3

--- Lesson3/exercise3.py
+++ Lesson3/exercise3.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 import json
 
+ipv4 =[]
+ipv6 = []
+
 f = open("exercise3.json")
 output= f.read()
 print("Printing JSON file")
@@ -11,31 +14,21 @@ output1 = json.load(f)
 print("Printing python format")
 pprint(output1)
 
-IP1_v4 = (output1['Ethernet2/1']['ipv4'])
-print(IP1_v4)
-IP2_v4 = (output1['Ethernet2/2']['ipv4'])
-print(IP2_v4)
-IP3_v4 = (output1['Ethernet2/3']['ipv4'])
-print(IP3_v4)
-
-IP1_v6 = (output1['Ethernet2/3']['ipv6'])
-print(IP1_v6)
-IP2_v6 = (output1['Ethernet2/4']['ipv6'])
-print(IP2_v6)
-
-ipv4 = []
-ipv6 = []
-
-ipv4.append(IP1_v4)
-ipv4.append(IP2_v4)
-ipv4.append(IP3_v4)
+for i in output1.values():
+    try:
+        ipv4.append(i['ipv4'])
+    except KeyError:
+        print()
+    try:
+        ipv6.append(i['ipv6'])
+    except KeyError:
+        print()
+    
 print("\n")
 print("Ipv4 addresses")
 print("^"*60)
 pprint(ipv4)
 
-ipv6.append(IP1_v6)
-ipv6.append(IP2_v6)
 
 print("\n")
 print("Ipv6 addresses")
