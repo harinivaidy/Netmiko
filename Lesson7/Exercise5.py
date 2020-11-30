ff924c851fd95573d4d95a5214262c447893bdfe
Harini Vaidyanathan <harinivaidy2001@gmail.com>
2020-11-30 05:34:55 -0800

    Updated Exercise 5

--- Lesson7/Exercise5.py
+++ Lesson7/Exercise5.py
@@ -8,12 +8,9 @@ my_xml = etree.fromstring(output)
 pprint(my_xml)
 print("\n")
 
-ns_map = {}
-ns_map[None] = "http://www.cisco.com/nxos:1.0:sysmgrcli"
-ns_map['nf'] = "urn:ietf:params:xml:ns:netconf:base:1.0"
-
-k = my_xml.find("nf:data", namespaces=ns_map)
+k = my_xml.nsmap
 print(k)
+
 print("\n")
-print(k.find(".//{*}proc_board_id").text)
+print(my_xml.find(".//{*}proc_board_id").text)
 print("\n")
