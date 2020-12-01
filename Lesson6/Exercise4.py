0651d9be4866d8b1bc4b8134db70ddb68cb30c6f
Harini Vaidyanathan <harinivaidy2001@gmail.com>
2020-11-30 16:43:53 -0800

    Adding Exercise 4 in Lesson6

--- Lesson6/Exercise4.py
+++ Lesson6/Exercise4.py
@@ -2,19 +2,41 @@ import pyeapi
 import yaml
 from pprint import pprint
 from getpass import getpass
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
+
+env = Environment()
+env.loader =  FileSystemLoader('.')
 
 f = open("Exercise4.yml")
 output = yaml.load(f)
 pprint(output)
 print(type(output))
 
-for CONN in output.values():
+for TEMPLATE,CONN in output.items():
+    # Code to prepare the template #
+    data = output[TEMPLATE]['data']
+    pprint(data)
+    print("\n")
+    print("Template for the Device")
+    print("-"*30)
+    template = env.get_template('Exercise4.j2')
+    output1 = template.render(**data)
+    print(output1)
+    output1 = output1.splitlines()
+    pprint(output1)
+    print(type(output1))
+    print("\n")
+    
     CONN['password'] = getpass()
     connection = pyeapi.client.connect(**CONN)
     device = pyeapi.client.Node(connection)
     print(device)
+    print("Printing Device model")
+    print("-"*30)
     print(device.model)
-
-
+    output3 = device.config(output1)
+    print(output3)
+    print("\n")
 
